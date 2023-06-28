e3a3bc2c847e49e81931835d7ed69a11119a0b17
Jan Albert Sobreo <janalbert@globalsaassol.com>
2023-06-28 21:38:59 +0800

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
+// export default defineNuxtConfig({
+//     modules: ['@nuxtjs/tailwindcss'],
+//     app: {
+//         pageTransition: {
+//             name: 'page',
+//             mode: 'out-in' // default
+//         },
+//         layoutTransition: {
+//             name: 'slide',
+//             mode: 'out-in' // default
+//         }
+//     }
+// })
+import { fileURLToPath } from "node:url"
+import fs from 'fs'
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-adsense'],
     app: {
         pageTransition: {
             name: 'page',
@@ -9,24 +24,16 @@ export default defineNuxtConfig({
             name: 'slide',
             mode: 'out-in' // default
         }
+    },
+    'google-adsense': {
+        id: 'ca-pub-1472925723025064'
+    },
+    devServer: {
+        host: "thecosmodood.com",
+        port: 443,
+        https: {
+            key: fs.readFileSync('/etc/letsencrypt/live/thecosmodood.com/privkey.pem').toString(),
+            cert: fs.readFileSync('/etc/letsencrypt/live/thecosmodood.com/fullchain.pem').toString()
+        }
     }
 })
-// import { fileURLToPath } from "node:url"
-// import fs from 'fs'
-// export default defineNuxtConfig({
-//     modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-adsense'],
-//     app: {
-//         pageTransition: { name: 'page', mode: 'out-in' }
-//     },
-//     'google-adsense': {
-//         id: 'ca-pub-1472925723025064'
-//     },
-//     devServer: {
-//         host: "thecosmodood.com",
-//         port: 443,
-//         https: {
-//             key: fs.readFileSync('/etc/letsencrypt/live/thecosmodood.com/privkey.pem').toString(),
-//             cert: fs.readFileSync('/etc/letsencrypt/live/thecosmodood.com/fullchain.pem').toString()
-//         }
-//     }
-// })
