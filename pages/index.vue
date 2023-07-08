6d42d83b913f61bcc065d8460331f41d3296e139
Jan Albert Sobreo <janalbert@globalsaassol.com>
2023-07-08 14:08:57 +0800

    updates

--- pages/index.vue
+++ pages/index.vue
@@ -138,7 +138,11 @@ export default{
                 this.minute = this.clock.getMinutes()
                 this.seconds = this.clock.getSeconds()
                 this.meridian = this.hour > 12 ? "PM" : "AM"
+
                 this.hour = this.hour % 12 || 12
+                this.hour = ("0" + this.hour).slice(-2)
+                this.minute = ("0" + this.minute).slice(-2)
+                this.seconds = ("0" + this.seconds).slice(-2)
             }, 1000)
         }
     },
